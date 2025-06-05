4dfab397fb600ced1e9e50bb77253e9b9a272778
Tiwari <tirshiva@amazon.com>
2025-06-05 22:53:37 +0530

    Loading model files from google drive

--- api/index.py
+++ api/index.py
@@ -5,14 +5,33 @@ from fastapi.staticfiles import StaticFiles
 import joblib
 import numpy as np
 import os
+import joblib
+import os
+import requests
 
 app = FastAPI(title="House Price Predictor", version="1.0.0")
 
 templates = Jinja2Templates(directory="templates")
 
 MODEL_VERSION = "v1"
-model = joblib.load(f"models/model_{MODEL_VERSION}.pkl")
-scaler = joblib.load(f"models/scaler_{MODEL_VERSION}.pkl")
+
+def download_file(url, filename):
+    if not os.path.exists(filename):
+        print(f"Downloading {filename}...")
+        r = requests.get(url)
+        with open(filename, 'wb') as f:
+            f.write(r.content)
+
+# ✅ Use your direct download links
+MODEL_URL = "https://drive.google.com/file/d/1U7GVnQQLVel5Uy9iQmtd7QqYWImNTxv2/view?usp=drive_link"
+SCALER_URL = "https://drive.google.com/file/d/1B1yVn9R5vdgkAl7lyN2HTv5h1KUkyUu5/view?usp=drive_link"
+
+download_file(MODEL_URL, "model_v1.pkl")
+download_file(SCALER_URL, "scaler_v1.pkl")
+
+# ✅ Load the model
+model = joblib.load("model_v1.pkl")
+scaler = joblib.load("scaler_v1.pkl")
 
 # ✅ HTML Form Route
 @app.get("/", response_class=HTMLResponse)
